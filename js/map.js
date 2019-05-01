b9e0db78c5776951e680b2b7b95919aca513a86c
ekim <44304148+play-chicken@users.noreply.github.com>
2019-05-01 01:02:35 -0500

    map

--- js/map.js
+++ js/map.js
@@ -1,9 +1,26 @@
+var marker;
 function initMap() { 
     var map = new google.maps.Map(document.getElementById("map"), {
         center: {lat: 41.881, lng: -87.623}, 
-        zoom: 8    
+        zoom: 13    
     });
     
+    marker = new google.maps.Marker({
+        map: map,
+        draggable: true,
+        animation: google.maps.Animation.DROP,
+        position: {lat: 41.8663, lng: -87.6068}
+    });
+    marker.addListner('click', toggleBounce);
+}
+
+function toggleBounce() {
+    if (marker.getAnimation() !== null) {
+    marker.setAnimation(null);
+  } else {
+    marker.setAnimation(google.maps.Animation.BOUNCE);
+  }
+
 }
 google.maps.event.addDomListener(window, 'load ', init);
 
